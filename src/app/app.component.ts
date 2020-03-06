7ddaca03d5ca84ca759d9bb9b4506b5a3350f26e
Рамин Камалов <kamalov@nemo.local>
2020-03-06 14:22:13 +0300

    from file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, SimpleChanges, OnInit,AfterViewChecked } from '@angular/core';
+import {Sort} from '@angular/material/sort';
+import { HttpClient} from '@angular/common/http';
+
+export interface Dessert {
+  calories: number;
+  carbs: number;
+  fat: number;
+  name: string;
+  protein: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit,AfterViewChecked{
   title = 'griddataapp';
+
+  // desserts: Dessert[] = [
+  //   {name: 'Frozen yogurt', calories: 159, fat: 6, carbs: 24, protein: 4},
+  //   {name: 'Ice cream sandwich', calories: 237, fat: 9, carbs: 37, protein: 4},
+  //   {name: 'Eclair', calories: 262, fat: 16, carbs: 24, protein: 6},
+  //   {name: 'Cupcake', calories: 305, fat: 4, carbs: 67, protein: 4},
+  //   {name: 'Gingerbread', calories: 356, fat: 16, carbs: 49, protein: 4},
+  // ];
+  desserts: Dessert[] = [];
+  sortedData: Dessert[];
+  
+  constructor(private http: HttpClient) {
+    
+  }
+
+  ngAfterViewChecked(){
+    //this.http.get('assets/tableGridData.json').subscribe((data: Dessert[]) => this.desserts=data);
+  }
+  ngOnInit(){
+    this.setDatas();
+    this.sortedData = this.desserts.slice();
+    //this.setDatas();
+    //console.log("datas="+this.);
+    //this.http.get('assets/tableGridData.json').subscribe((data: Dessert[]) => this.desserts=data);
+  }
+
+  setDatas(){
+    //this.desserts=[{name: 'Frozen yogurt XXX', calories: 159, fat: 6, carbs: 24, protein: 4}];
+    this.http.get('assets/tableGridData.json').subscribe((data: Dessert[]) => {this.desserts=data ; this.sortedData = this.desserts.slice();});
+  }
+
+  sortData(sort: Sort) {
+    const data = this.desserts.slice();
+    if (!sort.active || sort.direction === '') {
+      this.sortedData = data;
+      return;
+    }
+
+    this.sortedData = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'name': return compare(a.name, b.name, isAsc);
+        case 'calories': return compare(a.calories, b.calories, isAsc);
+        case 'fat': return compare(a.fat, b.fat, isAsc);
+        case 'carbs': return compare(a.carbs, b.carbs, isAsc);
+        case 'protein': return compare(a.protein, b.protein, isAsc);
+        default: return 0;
+      }
+    });
+  }
+}
+
+
+function compare(a: number | string, b: number | string, isAsc: boolean) {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
+import {MatSortModule} from '@angular/material/sort';
+import { HttpClientModule }   from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -11,7 +14,9 @@ import { MatSliderModule } from '@angular/material/slider';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MatSliderModule,
+    MatSortModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
