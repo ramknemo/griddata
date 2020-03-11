b9b779065af5d6d1c9aff4946f1cab73c8c5f80e
Рамин Камалов <kamalov@nemo.local>
2020-03-11 17:03:23 +0300

    resizing and colors going fixing

--- src/app/directives/row.color.directive.ts
+++ src/app/directives/row.color.directive.ts
@@ -6,8 +6,13 @@ export class RowColorDirective {
     { }
     private usedColor:string = "rgb(236, 233, 233)";
     @Input() set rowColorAttr(index: number) {
+        
          if (index%2==0) {
-            this.renderer.setStyle(this.elementRef.nativeElement, "background-color", this.usedColor);
+
+            //this.renderer.setStyle(this.elementRef.nativeElement, "background-color", this.usedColor);
+            //console.log("dir: "+index);
+        }else{
+
         }
     }
 }
